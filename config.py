4c10e38c626e5862122d56432b0e11a29100ca02
Halley <blackmoon8668@gmail.com>
2022-04-27 15:54:31 +0700

    first commit

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+
+app_dir = os.path.abspath(os.path.dirname(__file__))
+
+class BaseConfig:
+    DEBUG = True
+    POSTGRES_URL="mypostserver.postgres.database.azure.com"  
+    POSTGRES_USER="useracc@mypostserver" 
+    POSTGRES_PW="Maiyeuem@hp95"   
+    POSTGRES_DB="techconfdb"   
+    DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
+    CONFERENCE_ID = 1
+    SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://techconf-namespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=YD/j35ry932hGKdjTNBvf8jGUe39w1JyxnmA7eTiFHs=' 
+    SERVICE_BUS_QUEUE_NAME ='notificationqueue'
+    ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
+    SENDGRID_API_KEY = '' 
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
